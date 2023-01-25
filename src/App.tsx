1e92a0d359a030f1f723a5e41eaead24910f2301
Daniel S Goncalves <dsg1407@gmail.com>
2023-01-25 17:18:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./styles/global.css"
+
+export function App() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-background">
+      <h1 className="text-purple-600 text-5xl">Hello World</h1>
+    </div>
+  )
+}
