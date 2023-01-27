29dd7c53cb06289aecf4fb98d26b26e524d0609b
Daniel S Goncalves <dsg1407@gmail.com>
2023-01-26 23:47:31 -0300

    first structure and header design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import "./styles/global.css"
+import { Close, Ellipse } from "react-ionicons"
+
+import Avatar from "./assets/foto.jpg"
 
 export function App() {
   return (
-    <div className="flex h-screen justify-center items-center bg-background">
-      <h1 className="text-purple-600 text-5xl">Hello World</h1>
+    <div className="bg-background h-screen py-8 px-20 text-gray-200 font-roboto">
+      <header>
+        <div className="flex items-center justify-between gap-4">
+          <img
+            className="w-full max-w-[48px] rounded-full"
+            src={Avatar}
+            alt="Foto de avatar do chat"
+          />
+          <div className="w-full flex flex-col gap-1">
+            <h1 className="font-bold text-base">Cecilia Sassaki</h1>
+            <div className="flex items-center gap-1">
+              <Ellipse color="#00B37E" width={"10px"} />
+              <span className=" text-xs text-[#00B37E]">Online</span>
+            </div>
+          </div>
+          <button className="cursor-pointer">
+            <Close color="#FFFFFF" />
+          </button>
+        </div>
+      </header>
+      <main>
+        <div>MESSAGES</div>
+        <div>CHAT MENU</div>
+      </main>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: ["./src/**/*.tsx", "./index.html"],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ['"Roboto"', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
-        background: "#09090A",
+        background: "#1A1924",
+        boxBackground: "#282843",
       },
     },
   },
